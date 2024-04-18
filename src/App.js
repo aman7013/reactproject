89ff256647ba2ae839e503d2f19e53cec6ce346e
Aman Kumar Singh <kumaramansingh074@gmail.com>
2024-04-18 11:02:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Home from "./Components/Home";
+import Movie from "./Components/Movie";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Series from "./Components/Series";
+function App()
+{
+  return(
+    <>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Movie" element={<Movie/>}/>
+      <Route path="/Series" element={<Series/>}/>
+    </Routes>
+    </BrowserRouter>
+
+    </>
   );
 }
-
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,193 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+function Home()
+{
+
+    return(
+        <>
+        <Navbar/>
+
+
+<div className="slider">
+<div className="slider_bg">
+
+</div>
+<div className="slider_c1">
+<img src="../img/banner1.webp"></img>
+</div>
+<div className="slider_content fade-top">
+<h1 className="fade-top">BUMBLEBEE</h1>
+<p className="fade-top">
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-half-o" aria-hidden="true"></i>&nbsp;
+4.3/5 &nbsp; <span className="mbd">1MDB</span> &nbsp; <span className="action">ACTION</span> &nbsp; 2Hr:6 Mins 
+</p>
+<p className="fade-top lorem">Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiusmod tempor
+Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiusmod tempor
+Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiusmod tempor.
+</p>
+<p><span className="genres">Genres:</span>Action <br/>
+<span className="genres">Tags:</span>Action,Advanture,Horror</p>
+
+<p><span className="button">PLAY NOW <i class="fa fa-caret-right" aria-hidden="true"></i></span></p>
+</div>
+</div>
+
+
+  <div className="latest">
+    <div className="latest_c1">
+        <h2>Latest Movies</h2>
+        <div className="latest_row">
+            <div className="latest_c1_a">
+                <img src="../img/movie-01.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-03.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-04.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-05.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/comedy-series-1.png"></img>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+  <div className="latest">
+    <div className="latest_c1">
+        <h2>Horror Series</h2>
+        <div className="latest_row">
+            <div className="latest_c1_a">
+                <img src="../img/movie-11.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-02.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-12.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/horror-11.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/horror-12.jpg"></img>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+  <div className="latest">
+    <div className="latest_c1">
+        <h2>Old Movies</h2>
+        <div className="latest_row">
+            <div className="latest_c1_a">
+                <img src="../img/movie-01.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-03.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-04.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-05.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/comedy-series-1.png"></img>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+  <div className="never">
+<div className="never_bg">
+
+</div>
+<div className="never_c1">
+<img src="../img/never1.png"></img>
+</div>
+<div className="never_content fade-left">
+    <h2>Never Stop Looking </h2>
+    <h3>
+        <span className="">450Tk</span>
+        <span className="five"><s>500Tk</s></span>
+    </h3>
+    <p><span className="button">Watch Now</span></p>
+</div>
+</div>
+
+
+<div className="latest">
+    <div className="latest_c1">
+        <h2>Watch With Family</h2>
+        <div className="latest_row">
+            <div className="latest_c1_a">
+                <img src="../img/movie-06.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-09.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-10.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/horror-6.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/comedy-series-1.png"></img>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+  <div className="real">
+    <div className="real_c1">
+        <img src="../img/series-2.png"></img>
+    </div>
+  </div>
+
+   
+<div className="latest">
+    <div className="latest_c1">
+        <h2>Award Winning Movies</h2>
+        <div className="latest_row">
+            <div className="latest_c1_a">
+                <img src="../img/movie-06.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-09.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/movie-10.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/horror-6.jpg"></img>
+            </div>
+            <div className="latest_c1_a">
+                <img src="../img/comedy-series-1.png"></img>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+            <Footer/>
+        </>
+    )
+    
+}
+export default Home;
